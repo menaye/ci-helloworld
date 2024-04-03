5662badf68c55a9d44ed7b496296e40adbb738b1
menaye <menaye1@jhu.edu>
2024-04-02 20:38:42 -0400

    Added Gradle Java Project

---
+++ ci-helloworld/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+
+plugins {
+    id("java")
+    id("jacoco")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("net.jqwik:jqwik:1.8.3")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "Main"
+    }
+}
+
+tasks.jacocoTestReport {
+    reports {
+        csv.required = true
+    }
+}
+
+tasks.withType<Test> {
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events("started", "skipped", "passed", "failed")
+        showStandardStreams = true
+    }
+}
